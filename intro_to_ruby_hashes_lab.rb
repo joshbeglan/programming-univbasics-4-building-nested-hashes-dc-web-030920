16c54ffe773c1220ad609b50561dfc8fa12ddeeb
Josh Beglan <josh_beglan@yahoo.com>
2020-03-03 16:41:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,10 +34,12 @@ def monopoly_with_fourth_tier
         :four_pieces_owned => 200
     },
       :names => {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :shortline_railroad => {}}
+        :reading_railroad => {:mortgage_value => "$"},
+        :pennsylvania_railroad => {:mortgage_value => "$"},
+        :b_and_o_railroad => {:mortgage_value => "$"},
+        :shortline_railroad => {:mortgage_value => "$"}
+        
+      }
     }
   }
 end
