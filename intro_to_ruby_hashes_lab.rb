588a26b7d5e1787e49d240b1d6bea4e60a07e56c
Josh Beglan <josh_beglan@yahoo.com>
2020-03-03 16:33:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_third_tier
   {:railroads => {
       :pieces => 4,
       :rent_in_dollars => {:a => 1, :b => 2, :c => 3, :d => 4},
-      :names => 1
+      :names => {:e => 5, :f => 6, :g => 7, :h => 8}
     }
   }
 end
